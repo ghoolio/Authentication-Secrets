56f6fd91e3b5714b428c9dfb29b7d9aaed60db57
ghoolio <david.noeldner@icloud.com>
2023-12-20 01:46:38 +0100

    bcrypt encryption

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const ejs = require('ejs');
 const port = 3000;
 const pg = require('pg');
 const SHA256 = require("crypto-js/sha256");
+const bcrypt = require("bcrypt");
  
+const saltRounds = 10;
 const app = express();
  
 app.use(express.static('public'));
@@ -38,9 +40,11 @@ const secretKey = process.env.SECRETKEY;
 
 app.post("/register", async (req, res) => {
     const email = req.body.username;
-    const password = SHA256(req.body.password).toString();
+    const password = req.body.password;  // Note: Do not hash the password here with bcrypt
     try {
-        await db.query("INSERT INTO users (username, password) VALUES ($1, pgp_sym_encrypt(($2), ($3)))", [email, password, secretKey]);
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        console.log(hashedPassword);
+        await db.query("INSERT INTO users (username, password) VALUES ($1, $2)", [email, hashedPassword]);
         res.render("secrets.ejs");
     } catch (err) {
         console.log(err.message);
@@ -49,18 +53,27 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
     const email = req.body.username;
-    const password = SHA256(req.body.password).toString();
+    const password = req.body.password;
+
     try {
-        const result = await db.query("SELECT username, pgp_sym_decrypt(password, ($1)) AS password FROM users WHERE username=($2)", [secretKey, email]);
+        const result = await db.query("SELECT username, password FROM users WHERE username=($1)", [email]);
         const user = result.rows[0];
-        console.log(user);
-        if (user.password == password) {
-            res.render("secrets.ejs");
-        };
+        if (user) {
+            const match = await bcrypt.compare(password, user.password);
+            if (match) {
+                res.render("secrets.ejs");
+                return;
+            }
+        }
+        // Incorrect username or password
+        res.redirect("/login");
     } catch (err) {
         console.log(err.message);
     };
 });
+
+
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
