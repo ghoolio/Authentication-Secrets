943409737162b7c63bcd279e00ed09bd38ca9fe4
ghoolio <david.noeldner@icloud.com>
2023-12-22 00:29:12 +0100

    corrected passport version

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-// Import necessary packages
-
-
+// Load environment variables from .env file
 require('dotenv').config();
+
+// Use require for packages
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const ejs = require('ejs');
-const pg = require('pg');
-const bcrypt = require("bcrypt");
 const LocalStrategy = require('passport-local').Strategy;
-const { Pool } = pg;
- 
+const bcrypt = require('bcrypt');
+const { Pool } = require('pg');
+
 // Create a PostgreSQL pool for database connections
 const dbPool = new Pool({
     user: "postgres",
@@ -20,25 +18,25 @@ const dbPool = new Pool({
     password: process.env.PASSWORD,
     port: 5433,
 });
- 
+
 // Function to execute SQL queries on the database
 async function query(sql, params) {
     const client = await dbPool.connect();
     try {
         return await client.query(sql, params);
     } finally {
-        client.release(); 
+        client.release();
     }
 }
- 
+
 // Create an Express application
 const app = express();
 const port = 3000;
- 
+
 // Set up middleware for parsing URL-encoded request bodies and serving static files
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
- 
+
 // Configure session handling using express-session
 app.use(
     session({
@@ -53,89 +51,116 @@ app.use(
         },
     })
 );
- 
+
 // Initialize Passport and use Passport sessions
 app.use(passport.initialize());
 app.use(passport.session());
- 
-// Check if email exists in the database
-const emailExists = async (email) => {
-    // Query the database to check if the email exists
-    const data = await query("SELECT * FROM users WHERE email=$1", [email]);
-   
+
+// Check if username exists in the database
+const usernameExists = async (username) => {
+    // Query the database to check if the username exists
+    const data = await query("SELECT * FROM users WHERE username=$1", [username]);
+
     // Return the user data if found, otherwise return false
-    if (data.rowCount == 0) return false; 
-    return data.rows[0];
-};
- 
-// Create a new user in the database
-const createUser = async (email, password) => {
-    // Generate a salt and hash the password
-    const salt = await bcrypt.genSalt(10); // Set the number of salt rounds for password hashing
-    const hash = await bcrypt.hash(password, salt);
-   
-    // Insert the new user into the database and return the user data
-    const data = await query(
-        "INSERT INTO users(email, password) VALUES ($1, $2) RETURNING id, email, password",
-        [email, hash]
-    );
-   
-    // Return the newly created user
     if (data.rowCount == 0) return false;
     return data.rows[0];
 };
- 
-// Match entered password with the hashed password from the database
-const matchPassword = async (password, hashPassword) => {
-    // Compare the entered password with the hashed password
-    const match = await bcrypt.compare(password, hashPassword);
-    return match
+
+// Create a new user in the database
+const createUser = async (username, password) => {
+    try {
+        // Generate a salt and hash the password
+        const salt = await bcrypt.genSalt(10); // Set the number of salt rounds for password hashing
+        const hash = await bcrypt.hash(password, salt);
+
+        // Convert the hash buffer to a string
+        const hashString = hash.toString('utf-8');
+
+        // Insert the new user into the database and return the user data
+        const data = await query(
+            "INSERT INTO users(username, password) VALUES ($1, $2) RETURNING id, username, password",
+            [username, hashString]
+        );
+
+        // Return the newly created user
+        if (data.rowCount == 0) return false;
+        return data.rows[0];
+    } catch (error) {
+        console.error("Error during user creation:", error);
+        throw error;
+    }
 };
- 
+
+// Match entered password with the hashed password from the database
+const matchPassword = (password, hashPassword) => {
+    // Convert the hashPassword buffer to a string
+    const hashString = hashPassword.toString('utf-8');
+    // Use compare for asynchronous password comparison
+    return bcrypt.compare(password, hashString);
+};
+
 // Passport strategy for user registration
-passport.use("local-register", new LocalStrategy( async (username, password, done) => {
-    try {
-        // Check if the user already exists
-        const userExists = await emailExists(username)
- 
-        // If user exists, return false
-        if (userExists) {
-            return done(null, false);
+passport.use(
+    "local-register",
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            // Check if the user already exists
+            const userExists = await usernameExists(username);
+
+            // If user exists, return false
+            if (userExists) {
+                return done(null, false);
+            }
+
+            // Create a new user and return the user object
+            const user = await createUser(username, password);
+            return done(null, user);
+        } catch (error) {
+            done(error);
         }
- 
-        // Create a new user and return the user object
-        const user = await createUser(username, password);
-        return done(null, user);
-    } catch (error) {
-        done(error);
-    }
-}));
- 
+    })
+);
+
 // Passport strategy for user login
-passport.use("local-login", new LocalStrategy(async (username, password, done) => {
-    try {
-        // Find the user in the database
-        const user = await emailExists(username);
-        
-        // If user doesn't exist, return false
-        if (!user) return done(null, false);
-        
-        // Check if the password matches
-        const isMatch = await matchPassword(password, user.password);
-        
-        // Return user object if password matches, otherwise return false
-        if (!isMatch) return done(null, false);
-        return done(null, user);
-    } catch (error) {
-        return done(error, false);
-    }
-}));
- 
+passport.use(
+    "local-login",
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            // Find the user in the database
+            const user = await usernameExists(username);
+
+            // If user doesn't exist, return false
+            if (!user) {
+                console.log("User not found");
+                return done(null, false);
+            }
+
+            // Check user data before password comparison (debugging)
+            console.log("User data before password comparison:", user);
+
+            // Check if the password matches
+            const isMatch = await matchPassword(password, user.password);
+
+            // Return user object if password matches, otherwise return false
+            if (isMatch) {
+                console.log("Login successful");
+                return done(null, user);
+            } else {
+                console.log("Password does not match");
+                return done(null, false);
+            }
+        } catch (error) {
+            console.error("Error during login:", error);
+            return done(error, false);
+        }
+    })
+);
+
 // Serialize and deserialize user information for session management
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
-  
+
 passport.deserializeUser(async (id, done) => {
     try {
         // Find the user by ID and return the user object
@@ -146,55 +171,55 @@ passport.deserializeUser(async (id, done) => {
         done(error, false);
     }
 });
- 
+
 // Routes for different endpoints
- 
+
 // Home page route
 app.get("/", async (req, res) => {
     res.render("home.ejs");
 });
- 
+
 // Register page route
 app.get("/register", async (req, res) => {
     res.render("register.ejs");
 });
- 
+
 // Login page route
 app.get("/login", async (req, res) => {
     res.render("login.ejs");
 });
- 
+
 // Login form submission route
 app.post('/login', passport.authenticate('local-login', {
     failureRedirect: '/login', // Redirect on failure
     successRedirect: '/secrets', // Redirect on success
 }));
- 
+
 // Registration form submission route
 app.post("/register", passport.authenticate("local-register", {
     failureRedirect: '/register', // Redirect if registration fails
     successRedirect: '/secrets', // Redirect on successful registration
 }));
- 
+
 // Secrets page route
 app.get('/secrets', (req, res) => {
     // Check if the user is authenticated
     if (req.isAuthenticated()) {
         res.render('secrets.ejs');
     } else {
-        // Redirect to login page if user is not authenticated
+        // Redirect to login page if the user is not authenticated
         res.redirect('/login');
     }
 });
- 
+
 // Logout route
-app.get("/logout",(req,res)=>{
-    res.clearCookie("connect.sid"); // Clear the cookies left on client-side
-    req.logOut(()=>{
+app.get("/logout", (req, res) => {
+    res.clearCookie("connect.sid"); // Clear the cookies left on the client-side
+    req.logOut(() => {
         res.redirect("/"); // Redirect to the home page after logout
     });
 });
- 
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
